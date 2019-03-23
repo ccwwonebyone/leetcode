56c7ed2df6a6658d8481116488ac9a76953d6389
ccwwonebyone <ccwwonebyone@163.com>
2019-03-23 17:08:06 +0800

    Coin Change

---
+++ solution/coinChange.py
@@ -0,0 +1,34 @@
+class Solution(object):
+
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        if amount == 0:
+            return 0
+        value1 = [0]
+        value2 = []
+        nc =  0
+        visited = [False]*(amount+1)
+        visited[0] = True
+        while value1:
+            nc += 1
+            for v in value1:
+                for coin in coins:
+                    newval = v + coin
+                    if newval == amount:
+                        return nc
+                    elif newval > amount:
+                        continue
+                    elif not visited[newval]:
+                        visited[newval] = True
+                        value2.append(newval)
+            value1, value2 = value2, []
+        return -1
+
+if __name__ == '__main__':
+
+    solution = Solution()
+    print(solution.coinChange([1,2,5],11))
