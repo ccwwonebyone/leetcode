b82f62330d10e4b26039ecff64cd6acd664bef38
ccwwonebyone <ccwwonebyone@163.com>
2019-03-23 17:10:31 +0800

    Excel Sheet Column Title

---
+++ solution/convertToTitle.py
@@ -0,0 +1,18 @@
+class Solution:
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+        result = []
+        while n > 0:
+            result.append(capitals[(n-1)%26])
+            n = (n-1) // 26
+        result.reverse()
+        return ''.join(result)
+
+if __name__ == '__main__':
+
+    solution = Solution()
+    print(solution.convertToTitle(5))
