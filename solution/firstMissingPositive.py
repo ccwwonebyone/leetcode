b60058596ced2425f3124b8e561ce1698e873010
ccwwonebyone <ccwwonebyone@163.com>
2019-04-22 22:28:42 +0800

    First Missing Positive

---
+++ solution/firstMissingPositive.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 1
+        nums.sort()
+        i = 1
+        for num in nums:
+            if num > 0:
+                if num != i:
+                    if i not in nums:
+                        return i
+                    else:
+                        i += 1
+                else:
+                    i += 1
+        return i
+
+if __name__ == '__main__':
+
+    solution = Solution()
+    print(solution.firstMissingPositive([7,8,9,11,12])
