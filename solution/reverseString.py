acdbcc70492806a6278d48e124c57b75696a6cdb
ccwwonebyone <ccwwonebyone@163.com>
2019-11-18 11:02:14 +0800

    Reverse String

---
+++ solution/reverseString.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        the best
+        s,reverse()
+        """
+        return s[::-1]
+
+if __name__ == '__main__':
+
+    solution = Solution()
+    print(solution.reverseString('asdasdc'))
