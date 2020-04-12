9b24ee683d98e55b054cddb925e0845e48131ec3
ccwwonebyone <ccwwonebyone.163.com>
2020-04-12 21:45:23 +0800

    Palindrome Number

---
+++ solution/isPalindromeInt.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, x: int) -> bool:
+        s = str(x)
+        return s == s[::-1]
+
+if __name__ == "__main__":
+
+    solution = Solution()
+    print(solution.isPalindrome(2))
