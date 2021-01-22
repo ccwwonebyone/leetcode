450f6bb7cca281d7a4c18d34e2e03954bd2c4ed2
ccwwonebyone <ccwwonebyone@163.com>
2021-01-22 16:27:02 +0800

    maxArea|不要做无谓的计算

--- solution/maxArea.py
+++ solution/maxArea.py
@@ -3,16 +3,27 @@
 class Solution:
     
     def maxArea(self, height):
-        for i in range(len(height)-1, -1, -1):
-            pass
-        max_res = 0
-        for i in range(len(height)-1):
-            for j in range(i + 1, len(height)):
-                info = height[i] if height[i] < height[j] else height[j]
-                area = (j-i) * info
-                if max_res < area:
-                    max_res = area
-        return max_res
+        area = 0
+        left = 0
+        right = len(height) - 1
+        while left < right:
+            area = max(area, (right-left) * min(height[left],height[right]))
+            if height[left] > height[right]:
+                right -= 1
+            else:
+                left += 1
+        return area        
+       
+        # for i in range(len(height)-1, -1, -1):
+        #     pass
+        # max_res = 0
+        # for i in range(len(height)-1):
+        #     for j in range(i + 1, len(height)):
+        #         info = height[i] if height[i] < height[j] else height[j]
+        #         area = (j-i) * info
+        #         if max_res < area:
+        #             max_res = area
+        return area
 
 
 if __name__ == "__main__":
