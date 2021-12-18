281efcd2412567e4e6efe80e06014e4b12fc8693
kenan <kenan@haoshengmall.com>
2021-12-18 18:00:41 +0800

    levelOrder

---
+++ solution/levelOrder.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+import collections
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            res.append(node.val)
+            if node.left: queue.append(node.left)
+            if node.right: queue.append(node.right)
+        return res
+
+
+if __name__ == '__main__':
+    solution = Solution()
